1d799ee017cf68d019e9159c183327a8074ceed9
Adrian reyes <adriandmc05@gmail.com>
2023-08-09 17:06:53 -0400

    use next cookies to validate auth token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { theme } from "@/theme-provider";
 import { ThemeProvider } from "@mui/material";
 import dayjs from "dayjs";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import NavBar from "@/components/navbar";
+import React from "react";
+import { getCookie } from "cookies-next";
+
+const protectedRoutes = ["/treatments"];
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  dayjs.locale('es');
+  dayjs.locale("es");
 
   return (
     <ThemeProvider theme={theme}>
@@ -15,3 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
     </ThemeProvider>
   );
 }
+
+App.getInitialProps = async (context: AppContext) => {
+  const { ctx } = context;
+
+  const token = getCookie("token", { req: ctx.req, res: ctx.res });
+
+  if (!token && protectedRoutes.includes(ctx.pathname)) {
+    ctx?.res?.writeHead(302, {
+      Location: "/login",
+    });
+    ctx?.res?.end();
+  }
+
+  return {
+    props: {},
+  };
+};
