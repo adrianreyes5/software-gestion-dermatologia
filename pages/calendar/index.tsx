6cf02aa2ceb895b05f93c43a73cf92fc39c7ccf5
Adrian reyes <adriandmc05@gmail.com>
2023-08-12 22:21:15 -0400

    add appoiment calendar

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -4,30 +4,58 @@ import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import { Appointment } from "@/utils/types";
+
+import axios from "../../src/config/interceptor";
+
+type CalendarList = {
+  title: string;
+  date: string;
+};
 
 export default function Calendar() {
+  const [appoiments, setAppoiments] = React.useState<CalendarList[]>([]);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`/appointments`);
+        const data: Appointment[] = response.data.data;
+
+        const calendarList = data.map((item) => ({
+          title: item.treatment,
+          date: `${item.date} ${item.start_time?.slice(0, 5)}`,
+        }));
+
+        setAppoiments(calendarList);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getData();
+  }, []);
+
   const handleDate = (event: any) => {
     console.log(event);
   };
 
   return (
-    <Box maxWidth="1200px" margin="100px auto">
+    <Box width="1600px" margin="100px auto">
       <Paper
         style={{
           fontFamily: "Roboto",
+          width: "100%",
+          height: 1000,
         }}
       >
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
           locale="es"
-          events={[
-            { title: "event 1", date: "2023-08-01 10:00" },
-            { title: "event 1", date: "2023-08-01 10:30" },
-            { title: "event 2", date: "2023-08-02" },
-          ]}
+          events={appoiments}
           eventClick={handleDate}
-          
+          height={1000}
         />
       </Paper>
     </Box>
